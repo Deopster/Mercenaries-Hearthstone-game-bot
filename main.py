5fc39a8d44619287828cc2db601a9cfd56ef48e6
Deopster <68296704+Deopster@users.noreply.github.com>
2021-10-17 01:44:13 +0300

    add battlefuncs

--- main.py
+++ main.py
@@ -23,14 +23,45 @@ def findgame():
      else:
          print("Игра не найдена")
          return False
+def set():
+    while True:
+        if find_ellement('num.png'):
+            break
+    print(win.rect)
+    x = win.rect[2]/2.85
+    y = win.rect[3]-win.rect[3]/10
+    while True:
+        ahk.mouse_position = (x, y)
+        time.sleep(0.5)
+        if find_ellement('shab.png'):
+            ahk.mouse_drag(x, y-500, relative=False)
+        x += win.rect[2] / 23
+
+def battlego():
+    print("Битва")
+    time.sleep(1)
+    find_ellement('battle.png')
+    find_ellement('start.png')
+    time.sleep(0.5)
+    find_ellement('sec.png')
+    time.sleep(0.5)
+    find_ellement('level.png')
+    find_ellement('start.png')
+    time.sleep(0.5)
+    find_ellement('goto.png')
+    find_ellement('start.png')
+    time.sleep(3)
+    find_ellement('play.png')
+    set()
 
 def where():
     find_ellement('join_button.png')
-    time.sleep(1)
+    time.sleep(0.5)
     find_ellement('group.png')
     find_ellement('back.png')
     return True
 def group_create():
+    time.sleep(1)
     if find_ellement('group_find.png') ==6:
         find_ellement('create.png')
         time.sleep(1)
@@ -50,13 +81,33 @@ def group_create():
         find_ellement('page_1.png')
         group_create()
     else:
-        i=0
-        while i<3:
-            if find_ellement('one.png') !=5:
-                find_ellement('drop.png')
-                i+=1
+        time.sleep(1)
+        if find_ellement('empty_check.png') == True:
+            find_ellement('level_check.png')
+            if find_ellement('30lvl.png') == False:
+                find_ellement('ready.png')
+                find_ellement('ready.png')
+                time.sleep(0.5)
+                find_ellement('goto.png')
+                time.sleep(0.5)
+                find_ellement('del.png')
+                find_ellement('ok.png')
+                group_create()
             else:
-                find_ellement('next.png')
+                find_ellement('ready.png')
+                find_ellement('ready.png')
+                find_ellement('back.png')
+                battlego()
+
+
+        else:
+            i=0
+            while i<3:
+                if find_ellement('one.png') !=6:
+                    find_ellement('drop.png')
+                    i+=1
+                else:
+                    find_ellement('next.png')
 
         find_ellement('ready.png')
 
@@ -77,21 +128,30 @@ def find_ellement(file):
         x=(pt[0]*2+w)/2
         y=(pt[1]*2+h)/2
         print("Обнаружен "+file,x,y)
-        if (file == "deff.png" or file == 'tiranda.png' or file == 'milhaus.png' or file == 'one.png'):
+        if (file == "deff.png" or file == 'tiranda.png' or file == 'milhaus.png' or file == 'one.png'or file == 'shab.png'):
             global xm
             global ym
             xm=x
             ym=y
             return True
         if file == "drop.png":
-            ahk.mouse_move(xm, ym, speed=10)
+            ahk.mouse_move(xm, ym, speed=5)
             time.sleep(1)
             ahk.mouse_drag(x, y, relative=False)
             return True
-        ahk.mouse_move(x, y, speed=10)  # Moves the mouse instantly to absolute screen position
-        ahk.click()  # Click the primary mouse button
-        if file == 'group_find.png':
+        if file == 'level_check.png':
+            ahk.mouse_move(x, y+70, speed=5)  # Moves the mouse instantly to absolute screen position
+            ahk.click()
             return True
+        if file =='goto.png':
+            ahk.mouse_move(x, y, speed=5)
+            return True
+        if file == '30lvl.png':
+            return False
+        if file =='num.png':
+            return True
+        ahk.mouse_move(x, y, speed=5)  # Moves the mouse instantly to absolute screen position
+        ahk.click()  # Click the primary mouse button
         if file == 'group.png':
             group_create()
     else:
@@ -99,7 +159,20 @@ def find_ellement(file):
         if file == 'group_find.png':
             return 6
         if file == 'one.png':
-            return 5
+            return 6
+        if file == 'empty_check.png':
+            return True
+
+        if file =='30lvl.png':
+            return True
+        if file =='sec.png':
+            return True
+        if file == 'num.png':
+            return False
+        if file == 'shab.png':
+            return False
+        if (file !='join_button.png' and file !='back.png' and file !='group.png'):
+            where()
 
 
 def main():
@@ -109,9 +182,7 @@ def main():
     win.maximize()
     win.to_top()
     win.maximize()
-    while True:
-        if findgame():
-            where()
+    set()
 
 if __name__ == '__main__':
     main()
