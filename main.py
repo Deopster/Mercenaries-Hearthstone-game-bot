b285ea3a3540e4358e3bbc17f22eaf163f7f81af
Deopster <68296704+Deopster@users.noreply.github.com>
2021-10-17 15:13:51 +0300

    shit code replaced
    now all working throw lists

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ Ui_Ellements=['battle','blue','green','group','next','one','page_1','page_2','pa
 #buttons
 buttons=['back','continue','create','del','join_button','num','ok','play','ready','sec','sta','start','start1','submit']
 #chekers
-chekers=['30lvl','empty_check','find','goto','group_find','level_check','rename','shab']
+chekers=['30lvl','empty_check','find','goto','group_find','level_check','rename','shab','drop']
 #levels
 levels=['level15']
 #heroes
@@ -30,27 +30,38 @@ hero=['deff','milhaus','tiranda']
 
 
 def configread():
+    global speed
+    global monik
     config = configparser.ConfigParser()
     config.read("settings.ini")
-    monik = (config["BotSettings"]["monitor"]).split("#")[0]
-    speed = (config["BotSettings"]["bot_speed"]).split("#")[0]
+    monik = int((config["BotSettings"]["monitor"]).split("#")[0])
+    speed = float((config["BotSettings"]["bot_speed"]).split("#")[0])
     print(monik,speed)
 def parslist():
     i=0
     while i<len(Ui_Ellements):
         Ui_Ellements[i]="UI_ellements/"+Ui_Ellements[i]+".png"
+        i+=1
+    print("1 блок")
     i = 0
     while i<len(buttons):
         buttons[i]="buttons/"+buttons[i]+".png"
+        i += 1
+    print("2 блок")
     i = 0
     while i<len(chekers):
         chekers[i]="chekers/"+chekers[i]+".png"
+        i += 1
     i = 0
+    print("3 блок")
     while i<len(levels):
         levels[i]="levels/"+levels[i]+".png"
+        i += 1
     i = 0
+    print("4 блок")
     while i<len(hero):
         hero[i]="heroes/"+hero[i]+".png"
+        i += 1
     return 0
 def screen():
     sct = mss.mss()
@@ -66,7 +77,7 @@ def findgame():
          return False
 def set():
     while True:
-        if find_ellement('num.png',0):
+        if find_ellement(buttons[5],1):
             break
     print(win.rect)
     x = win.rect[2]/2.85
@@ -74,7 +85,7 @@ def set():
     i=0
     while i<3:
         ahk.mouse_position = (x, y)
-        if find_ellement('shab.png',0):
+        if find_ellement(chekers[7],1):
             ahk.mouse_drag(x, y-500, relative=False)
             i+=1
         x += win.rect[2] / 23
@@ -84,86 +95,91 @@ def set():
 def battlego():
     print("Битва")
     time.sleep(1)
-    find_ellement('UI-ellements/battle.png',0)
+    find_ellement(Ui_Ellements[0],0)
     time.sleep(0.5)
-    if find_ellement('play.png',0):
+    if find_ellement(buttons[7],1):
         set()
     time.sleep(0.5)
-    find_ellement('sta.png',0)
+    find_ellement(buttons[10],0)
     time.sleep(1)
-    find_ellement('sec.png',0)
+    find_ellement(buttons[9],2)
     time.sleep(0.5)
-    find_ellement('level.png',0)
+    find_ellement(levels[0],0)
     time.sleep(0.5)
-    find_ellement('start.png',0)
+    find_ellement(buttons[11],0)
     time.sleep(0.5)
-    find_ellement('find.png',0)
+    find_ellement(chekers[2],0)
     time.sleep(0.5)
-    find_ellement('start1.png',0)
+    find_ellement(buttons[12],0)
     time.sleep(0.5)
-    if find_ellement('submit.png',0)!=False:
+    if find_ellement(buttons[13],1)!=False:
         time.sleep(0.5)
     time.sleep(5)
-    find_ellement('play.png',0)
+    find_ellement(buttons[7],0)
     set()
 
 def where():
-    find_ellement('join_button.png',0)
+    find_ellement(buttons[4],0)
     time.sleep(0.5)
-    find_ellement('group.png',0)
-    find_ellement('back.png',0)
+    find_ellement(Ui_Ellements[3],0)
+    find_ellement(buttons[0],0)
     return True
 def group_create():
     time.sleep(1)
-    if find_ellement('group_find.png',0) ==6:
-        find_ellement('create.png',0)
+    if find_ellement(chekers[4],3) ==6:
+        find_ellement(buttons[2],0)
         time.sleep(1)
-        find_ellement('rename.png',0)
+        find_ellement(chekers[6],0)
         ahk.send_input('Botwork',0)
-        if find_ellement('deff.png',0):
-            find_ellement('drop.png',0)
-        find_ellement('page_2.png',0)
-        find_ellement('next.png',0)
-        if find_ellement('tiranda.png',0):
-            find_ellement('drop.png',0)
-        find_ellement('page_3.png',0)
-        if find_ellement('milhaus.png',0):
-            find_ellement('drop.png',0)
-        find_ellement('ready.png',0)
-        find_ellement('continue.png',0)
-        find_ellement('page_1.png',0)
+        if find_ellement(hero[0],0):
+            find_ellement(chekers[8],0)
+        find_ellement(Ui_Ellements[7],0)
+        find_ellement(Ui_Ellements[4],0)
+        if find_ellement(hero[2],0):
+            find_ellement(chekers[8],0)
+        find_ellement(Ui_Ellements[8],0)
+        if find_ellement(hero[1],0):
+            find_ellement(chekers[8],0)
+        find_ellement(buttons[8],0)
+        find_ellement(buttons[1],0)
+        find_ellement(Ui_Ellements[6],0)
         group_create()
     else:
         time.sleep(1)
-        if find_ellement('empty_check.png',0) == True:
-            find_ellement('level_check.png',0)
-            if find_ellement('30lvl.png',0) == False:
-                find_ellement('ready.png',0)
-                find_ellement('ready.png',0)
+        if find_ellement(chekers[1],2) == True:
+            find_ellement(chekers[5],0)
+            if find_ellement(chekers[0],1) == True:
+                find_ellement(buttons[8],0)
+                find_ellement(buttons[8],0)
                 time.sleep(0.5)
-                find_ellement('goto.png',0)
+                find_ellement(chekers[3],0)
                 time.sleep(0.5)
-                find_ellement('del.png',0)
-                find_ellement('ok.png',0)
+                find_ellement(buttons[3],0)
+                find_ellement(buttons[6],0)
                 group_create()
             else:
-                find_ellement('ready.png',0)
-                find_ellement('ready.png',0)
-                time.sleep(0.5)
-                find_ellement('back.png',0)
+                find_ellement(buttons[8],0)
+                time.sleep(1)
+                find_ellement(buttons[8],0)
+                time.sleep(1)
+                find_ellement(buttons[0],0)
                 battlego()
 
 
         else:
             i=0
             while i<3:
-                if find_ellement('one.png',0) !=6:
-                    find_ellement('drop.png',0)
+                if find_ellement(Ui_Ellements[5],3) !=6:
+                    find_ellement(chekers[8],0)
                     i+=1
                 else:
-                    find_ellement('next.png',0)
+                    find_ellement(Ui_Ellements[4],0)
 
-        find_ellement('ready.png',0)
+        find_ellement(buttons[8],0)
+        time.sleep(1)
+        find_ellement(buttons[0], 0)
+        time.sleep(2)
+        battlego()
 def text():
     img = cv2.imread('files/screen.png')
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -175,7 +191,7 @@ def text():
 def find_ellement(file,index):
     time.sleep(speed)
     screen()
-    if index==3:
+    if index=="text":
         text()
     img = cv2.imread('files/screen.png')  # картинка, на которой ищем объект
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # преобразуем её в серуюш
@@ -191,63 +207,50 @@ def find_ellement(file,index):
         x=(pt[0]*2+w)/2
         y=(pt[1]*2+h)/2
         print("Обнаружен "+file,x,y)
-        if (file == "deff.png" or file == 'tiranda.png' or file == 'milhaus.png' or file == 'one.png'or file == 'shab.png'):
+        if (file == hero[0] or file == hero[1] or file == hero[2] or file == Ui_Ellements[5] or file == chekers[7]):
             global xm
             global ym
             xm=x
             ym=y
             return True
-        if file == "drop.png":
+        if file == chekers[8]:
             ahk.mouse_move(xm, ym, speed=5)
             time.sleep(1)
             ahk.mouse_drag(x, y, relative=False)
             return True
-        if file == 'level_check.png':
+        if file == chekers[5]:
             ahk.mouse_move(x, y+70, speed=3)  # Moves the mouse instantly to absolute screen position
             ahk.click()
             return True
-        if file =='goto.png':
+        if file ==buttons[5]:
             ahk.mouse_move(x, y, speed=5)
             return True
-        if file == '30lvl.png':
-            return False
-        if file =='num.png':
+        if index == 1:
             return True
         ahk.mouse_move(x, y, speed=5)  # Moves the mouse instantly to absolute screen position
         ahk.click()  # Click the primary mouse button
-        if file =='play.png':
+        if file ==buttons[7]:
             return True
-        if file == 'group.png':
+        if file == Ui_Ellements[3]:
             group_create()
     else:
-        print("объект не обнаружен"+file)
-        if file == 'group_find.png':
+        print("объект не обнаружен "+file)
+        if index== 3:
             return 6
-        if file == 'one.png':
-            return 6
-        if file == 'empty_check.png':
+        if index==2:
             return True
-
-        if file =='30lvl.png':
-            return True
-        if file =='sec.png':
-            return True
-        if file == 'num.png':
+        if index ==1 :
             return False
-        if file == 'shab.png':
-            return False
-        if file =='play.png':
-            return False
-        if file =='submit.png':
-            return False
-        if (file !='join_button.png' and file !='back.png' and file !='group.png'):
+        if (file !=buttons[4] and file !=Ui_Ellements[3] and file !=buttons[0]):
             where()
 
 
 def main():
+    ahk.show_info_traytip("Started", "loading files", slient=False, blocking=True)
     configread()
     findgame()
     parslist()
+    ahk.show_info_traytip("started", "all files loaded sucsessfuly", slient=False, blocking=True)
     win.show()
     win.restore()
     win.maximize()
