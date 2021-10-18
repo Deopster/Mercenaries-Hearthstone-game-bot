dc68616b083f522b404cee9510ff0b1f6a56ef0b
Deopster <68296704+Deopster@users.noreply.github.com>
2021-10-19 00:11:59 +0300

    battle system work begin

--- main.py
+++ main.py
@@ -15,12 +15,16 @@ ym=0
 global monik
 global speed
 global sens
+global zipp
+global zipchek
+zipp=False
+zipchek =False
 sens=0.75
 #for_future=['','','','','','','','','','','','','','','','','','','',]
 #Ui-ellements
-Ui_Ellements=['battle','blue','green','group','next','one','page_1','page_2','page_3','red','prev']
+Ui_Ellements=['battle','blue','green','group','next','one','page_1','page_2','page_3','red','prev','sob','noclass']
 #buttons
-buttons=['back','continue','create','del','join_button','num','ok','play','ready','sec','sta','start','start1','submit']
+buttons=['back','continue','create','del','join_button','num','ok','play','ready','sec','sta','start','start1','submit','allready']
 #chekers
 chekers=['30lvl','empty_check','find','goto','group_find','level_check','rename','shab','drop','301','302','taken','text']
 #levels
@@ -30,6 +34,12 @@ hero=['','','']
 hero_colour=['','','']
 pages=['','','']
 heroNUM=['','','']
+#for battle
+myhero=['','','']
+myherocol=['','','']
+#челы с молниями
+enemywiz=[0,0,0]
+enemywizF=['','','']
 #img list
 picparser=['/1.png','/2.png','/3.png','/4.png']
 
@@ -111,6 +121,46 @@ def findgame():
      else:
          print("Игра не найдена")
          return False
+
+
+def battle():
+    global zipp
+    global sens
+    global zipchek
+    global speed
+    zipp = False
+    ahk.mouse_move(100, 100, speed=3)  # Moves the mouse instantly to absolute screen position
+    ahk.click()
+    tmp=int(win.rect[3] / 2)
+    partscreen(2560, tmp, 0, 0)
+    temp = speed
+    speed = 0
+    enemyred = find_ellement(Ui_Ellements[9], 12)
+    enemygreen =find_ellement(Ui_Ellements[2], 12)
+    enemyblue=find_ellement(Ui_Ellements[1], 12)
+    enemynoclass=find_ellement(Ui_Ellements[12], 12)
+    print(enemyred,enemyblue,enemygreen,enemynoclass)
+    if zipchek==False:
+        if find_ellement(Ui_Ellements[11], 'battletag'):
+            zipchek = True
+    speed = temp
+    partscreen(2560, tmp, tmp, 0)
+    red = find_ellement(Ui_Ellements[9], 12)
+    green = find_ellement(Ui_Ellements[2], 12)
+    blue = find_ellement(Ui_Ellements[1], 12)
+    print(blue,red,green)
+    ahk.mouse_move(red, speed=3)
+    time.sleep(2)
+    ahk.mouse_move(blue, speed=3)
+    time.sleep(2)
+    ahk.mouse_move(green, speed=3)
+    time.sleep(2)
+    ahk.mouse_move(enemygreen, speed=3)
+    time.sleep(2)
+    ahk.mouse_move(enemynoclass, speed=3)
+    time.sleep(2)
+
+
 def set():
     global speed
     global sens
@@ -135,8 +185,9 @@ def set():
         if x>1700:
             x= win.rect[2]/2.85
     speed = temp
-    sens = 0.75
-
+    sens = 0.7
+    find_ellement(buttons[14], 1)
+    battle()
 def battlego():
     print("Битва")
     time.sleep(1)
@@ -293,23 +344,48 @@ def find_ellement(file,index):
     global left
     global Resolution
     time.sleep(speed)
-    if index ==7 and file != chekers[8] :
+    if index ==7 or index ==12 and file != chekers[8] :
         img = cv2.imread('files/'+Resolution+'/part.png')
     else:
         screen()
         img = cv2.imread('files/'+Resolution+'/screen.png')  # картинка, на которой ищем объект
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # преобразуем её в серуюш
     template = cv2.imread('files/'+Resolution+'/'+file,cv2.IMREAD_GRAYSCALE)  # объект, который преобразуем в серый, и ищем его на gray_img
-    w, h = template.shape[::-1]  # инвертируем из (y,x) в (x,y)
-
+    w, h = template.shape[::-1]  # инвертируем из (y,x) в (x,y)`
     result = cv2.matchTemplate(gray_img, template, cv2.TM_CCOEFF_NORMED)
+
     loc = np.where(result >= sens)
+    if index =='battletag':
+        print(loc)
+        if len(loc[0]) != 0:
+            j=0
+            for pt in zip(*loc[::-1]):
+                Flag=False
+                for num in range(3):
+                    if pt[0] < enemywiz[num] + 10 and pt[0] > enemywiz[num] - 10:
+                        Flag=True
+                        pass
+                if not Flag:
+                    for num in range(3):
+                        if enemywiz[num] == 0 :
+                            enemywiz[num]=pt[0]
+                            x = (pt[0] * 2 + w) / 2
+                            y = (pt[1] * 2 + h) / 2
+                            enemywizF[j]=x,y
+                            j+=1
+                            break
+            for i in range(2):
+                enemywiz[i]=0
+            return 0
     if len(loc[0]) !=0:
         for pt in zip(*loc[::-1]):
-            cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 1, 0), 3)
+            pt[0] + w
+            pt[1] + h
         x=(pt[0]*2+w)/2
         y=(pt[1]*2+h)/2
         print("Found "+file,x,y)
+        if index==12:
+            return x,y
         if (index==6 or file == Ui_Ellements[5] or file == chekers[7]):
             global xm
             global ym
@@ -345,6 +421,8 @@ def find_ellement(file,index):
             group_create()
     else:
         print("Not found  "+file)
+        if index == 12:
+            return 0
         if index ==6:
             return False
         if index ==7:
@@ -367,8 +445,7 @@ def main():
     findgame()
     parslist()
     ahk.show_info_traytip("started", "all files loaded sucsessfuly", slient=False, blocking=True)
-    set()
-
+    battle()
 '''
     win.show()
     win.restore()
