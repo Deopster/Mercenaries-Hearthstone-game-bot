bd73f3712aadf441a8e2acd4dfe4954878a21c3b
Deopster <68296704+Deopster@users.noreply.github.com>
2021-10-19 23:52:38 +0300

    repair drop

--- main.py
+++ main.py
@@ -455,7 +455,7 @@ def find_ellement(file,index):
     global left
     global Resolution
     time.sleep(speed)
-    if index ==7 or index ==12 and file != chekers[8] :
+    if (index ==7 or index ==12) and file != chekers[8] :
         img = cv2.imread('files/'+Resolution+'/part.png')
     else:
         screen()
